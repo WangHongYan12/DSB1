7e879da01ba9270ef6d4f935bced3fa441b35943
WHY_Laptop <2685699319@qq.com>
2025-05-24 14:38:20 +0800

    5/24

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -57,8 +57,8 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, CIN2_Pin|CIN1_Pin|DIN2_Pin|DIN1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PCPin PCPin PCPin PCPin
-                           PCPin */
+  /*Configure GPIO pins : AIN2_Pin AIN1_Pin BIN2_Pin BIN1_Pin
+                           USER_LED_Pin */
   GPIO_InitStruct.Pin = AIN2_Pin|AIN1_Pin|BIN2_Pin|BIN1_Pin
                           |USER_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -66,14 +66,14 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
+  /*Configure GPIO pins : CIN2_Pin CIN1_Pin DIN2_Pin DIN1_Pin */
   GPIO_InitStruct.Pin = CIN2_Pin|CIN1_Pin|DIN2_Pin|DIN1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : USER_KEY_Pin */
   GPIO_InitStruct.Pin = USER_KEY_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
